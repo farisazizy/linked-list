9c1af15eb38336ec833f49e77d03048f495ab9a2
Faris Azizy <55827840+farisazizy@users.noreply.github.com>
2020-03-28 14:17:50 +0700

    Add files via upload

---
+++ MovieReview/Child.h
@@ -0,0 +1,44 @@
+#ifndef CHILD_H_INCLUDED
+#define CHILD_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+#define next(P) P->next
+#define prev(P) P->prev
+#define first(L) L.first
+#define last(L) L.last
+#define info(P) P->info
+
+
+
+struct infotype_child{
+    string reviewer;
+};
+typedef struct elmlist_child *address_child;
+
+struct elmlist_child{
+    infotype_child info;
+    address_child next;
+    address_child prev;
+};
+
+struct List_child{
+    address_child first;
+    address_child last;
+};
+
+void createList(List_child &L);
+void insertFirst(List_child &L, address_child P);
+void insertLast(List_child &L, address_child P);
+void insertAfter(address_child Prec, address_child P);
+void deleteFirst(List_child &L, address_child &P);
+void deleteLast(List_child &L, address_child &P);
+void deleteAfter(address_child Prec, address_child &P);
+
+address_child alokasi(infotype_child x);
+void dealokasi(address_child &P);
+address_child findElm(List_child L, infotype_child x);
+void printInfo(List_child L);
+
+#endif // CHILD_H_INCLUDED
