93f448313b0d29f1d976f33479e2b54307f37c12
Faris Azizy <55827840+farisazizy@users.noreply.github.com>
2020-03-28 14:16:23 +0700

    Add files via upload

---
+++ Parents.h
@@ -0,0 +1,43 @@
+#ifndef PARENTS_H_INCLUDED
+#define PARENTS_H_INCLUDED
+#include <iostream>
+using namespace std;
+
+#define first(L) L.first
+#define next(P) P->next
+#define info(P) P->info
+
+// first last
+
+
+struct infotype_parent{
+    string movie;
+};
+
+typedef struct elmlist_parent *address_parent;
+
+struct elmlist_parent {
+    infotype_parent info;
+    address_parent next;
+};
+
+struct List_parent {
+    address_parent first;
+    address_parent last;
+};
+
+
+void createList(List_parent &L);
+void insertFirst(List_parent &L, address_parent P);
+void insertAfter(List_parent &L, address_parent Prec, address_parent P);
+void insertLast(List_parent &L, address_parent P);
+void deleteFirst(List_parent &L, address_parent &P);
+void deleteLast(List_parent &L, address_parent &P);
+void deleteAfter(List_parent &L, address_parent Prec, address_parent &P);
+
+address_parent alokasi(infotype_parent x);
+void dealokasi(address_parent &P);
+address_parent findElm(List_parent L, infotype_parent x);
+void printInfo(List_parent L);
+
+#endif // PARENTS_H_INCLUDED
---
+++ Relasi.cpp
@@ -0,0 +1,37 @@
+#include "Relasi.h"
+
+void createList(List_relasi &L){
+
+    first(L) = NULL;
+}
+void insertFirst(List_relasi &L, address_relasi P){
+
+}
+void insertLast(List_relasi &L, address_relasi P){
+
+}
+void insertAfter(address_relasi Prec, address_relasi P){
+
+}
+void deleteFirst(List_relasi &L, address_relasi &P){
+
+}
+void deleteLast(List_relasi &L, address_relasi &P){
+
+}
+void deleteAfter(address_relasi Prec, address_relasi &P){
+
+}
+
+address_relasi alokasi( address_parent P, address_child C){
+
+}
+void dealokasi(address_relasi &P){
+
+}
+address_relasi findElm(List_relasi L, address_parent P, address_child C){
+
+}
+void printInfo(List_relasi L){
+
+}
