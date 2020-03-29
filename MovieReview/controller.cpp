edc7d3375ab71498882fe265485824b048a9eb73
adityandar <62176741+adityaandar88@users.noreply.github.com>
2020-03-29 17:41:43 +0700

    added controller.cpp & controller.h

---
+++ MovieReview/controller.cpp
@@ -0,0 +1,30 @@
+#include "controller.h"
+
+void menu() {
+    cout<<"Menu: "<<endl;
+    cout<<"1. Login "<<endl;
+    cout<<"2. Sign Up"<<endl;
+    cout<<"3. View Only"<<endl;
+}
+
+void menu_admin() {
+    cout<<"Menu Admin: "<<endl;
+    cout<<"1. Insert film"<<endl;
+    cout<<"2. View film"<<endl;
+    cout<<"3. Update film "<<endl;
+    cout<<"4. Delete film "<<endl;
+    cout<<"5. View reviewer "<<endl;
+    cout<<"6. Delete reviewer "<<endl;
+    cout<<"7. View all review of specific film "<<endl;
+    cout<<"8. View all review of specific reviewer. "<<endl;
+}
+
+void menu_reviewer() {
+    cout<<"Menu Reviewer: "<<endl;
+    cout<<"1. Add review "<<endl;
+    cout<<"2. Delete review "<<endl;
+    cout<<"3. Show all of your review "<<endl;
+    cout<<"4. Show not reviewed movie "<<endl;
+    cout<<"5. Update reviewer's data "<<endl;
+}
+
